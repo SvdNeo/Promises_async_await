6834f48f4a5b8e55518a35ef3af8473fd4f64d72
SvdNeo <sanjay.d@neoito.com>
2023-12-29 15:32:40 +0530

    fetching array of urls

--- promiseApi.js
+++ promiseApi.js
@@ -8,3 +8,12 @@ const p1 = new Promise((resolve)=>setTimeout(() => {
     resolve(3)
   }, 1000))
   Promise.all([p1,p2,p3]).then(res=>console.log(res)).catch(err=>console.log(err)).finally(i=>console.log("All promises executed"))
+
+  let urls = [
+    'https://api.github.com/users/iliakan',
+    'https://api.github.com/users/remy',
+    'https://api.github.com/users/jeresig'
+  ];
+  let user = urls.map(i=>fetch(i))
+  console.log(user)
+  Promise.all(user).then(res=>res.forEach(i=>{console.log(`${i.url}:${i.status}`)}))
