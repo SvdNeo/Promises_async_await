688432b503ab2bbd14fef5876955ca803a5d3efc
SvdNeo <sanjay.d@neoito.com>
2024-01-03 11:19:11 +0530

    another example on async function

--- async_await.js
+++ async_await.js
@@ -18,3 +18,24 @@ const getData = async () => {
 console.log(1);
 getData();
 console.log(2);
+
+function asynchronous_operational_method() {
+	let first_promise = 
+		new Promise((resolve, reject) => resolve("Hello"));
+	let second_promise = 
+		new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve(" its a async function..");
+		}, 1000);
+	});
+	let combined_promise = 
+		Promise.all([first_promise, second_promise]);
+	return combined_promise;
+}
+
+async function display() {
+	let data = await asynchronous_operational_method();
+	console.log(data);
+}
+
+display();
