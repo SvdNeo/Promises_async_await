4a4a99e80c515cf84e77e91a5f0da2472c0b3501
SvdNeo <sanjay.d@neoito.com>
2024-01-03 13:36:01 +0530

    importing a function from another module

--- modules.js
+++ modules.js
@@ -1,5 +1,8 @@
-import { admin } from "./modules1.js";
+import { admin, sayHi } from "./modules1.js";
 
 console.log(admin.name)
 admin.name = "vinay";
 console.log(admin.name)
+import {config} from './modules1.js';
+config.user = "Pete";
+sayHi()
--- modules1.js
+++ modules1.js
@@ -1,1 +1,6 @@
 export let admin = {name:"sanjay"}
+export let config = { };
+
+export function sayHi() {
+  console.log(`Ready to serve, ${config.user}!`);
+}
